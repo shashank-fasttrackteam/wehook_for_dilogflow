0b031f06ec10849cac337743a859f73cea1ebfce
Shahsank Mane <shashank.mane@fasttrackteam.com>
2018-12-12 14:11:31 +0530

    Testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  post '/callback', to: 'webhooks#callback', constraints: { format: 'json' }
+  # post '/callback', to: 'webhooks#callback', constraints: { format: 'json' }
   mount ActionCable.server => '/cable'
   # constraints subdomain: "hooks" do
   #   post '/:integration_name' => 'webhooks#receive', as: :receive_webhooks
   # end
   post '/webhooks', to: 'webhooks#webhook', constraints: { format: 'json' }
-  root 'welcome#index'
+  get '/welcomes' to: 'welcomes/index'
+  root 'webhooks#webhook'
 end
