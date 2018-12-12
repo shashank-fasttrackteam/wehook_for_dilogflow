bb1bee0715bb183b0bc68d3366083b0e5231f51e
Shahsank Mane <shashank.mane@fasttrackteam.com>
2018-12-12 12:08:26 +0530

    syntax error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   #config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-  config.action_cable.allowed_request_origins = = [ 'https://18.216.180.230' ]
+  config.action_cable.allowed_request_origins = [ 'https://18.216.180.230' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
